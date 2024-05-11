9977ee535e2a9c729ae8d2c7dcc9d83c21979b70
60936 <studio34pl@gmail.com>
2024-05-11 08:47:08 +0800

    r

--- pdf.py
+++ pdf.py
@@ -2,7 +2,7 @@
 import tabula
 # Read a PDF File
 # make sure your pdf file is in the same directory as your notebook
-df = tabula.read_pdf("UDC Bank 202301.pdf", pages='all')[0]
+df = tabula.read_pdf("file.pdf", pages='all')[0]
 # convert PDF into CSV
-tabula.convert_into("UDC Bank 202301.pdf", "bank2301.csv", output_format="csv", pages='all')
+tabula.convert_into("file.pdf", "text.csv", output_format="csv", pages='all')
 print(df)
